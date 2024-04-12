864e77bee4300df1b6ab6a8fc954e2c5b6b34022
s.prusakov <s.prusakov@nv.dunice.net>
2024-04-12 19:21:51 +0300

    Made a request to update parameter isDone

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Patch, Post, Query } from '@nestjs/common';
 import { CreateTaskDto } from './dto/create-task-dto';
 import { TasksService } from './tasks.service';
 
@@ -6,13 +6,23 @@ import { TasksService } from './tasks.service';
 export class TasksController {
   constructor(private tasksService: TasksService) {}
 
+  @Get()
+  getAll() {
+    return this.tasksService.getAllTasks();
+  }
+
   @Post()
   create(@Body() taskDto: CreateTaskDto) {
     return this.tasksService.createTask(taskDto);
   }
 
-  @Get()
-  getAll() {
-    return this.tasksService.getAllTasks();
+  @Patch('/changeStatus')
+  changeStatus(@Query('id') id: any) {
+    return this.tasksService.changeStatusTask(id);
+  }
+
+  @Patch('/upgrade')
+  upgrade(@Query() params: any, @Body() dto: CreateTaskDto) {
+    return this.tasksService.upgradeTask(params, dto);
   }
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -7,13 +7,35 @@ import { CreateTaskDto } from './dto/create-task-dto';
 export class TasksService {
   constructor(@InjectModel(Task) private taskRepository: typeof Task) {}
 
+  async getAllTasks() {
+    const tasks = await this.taskRepository.findAll();
+    return tasks;
+  }
+
   async createTask(dto: CreateTaskDto) {
     const task = await this.taskRepository.create(dto);
     return task;
   }
 
-  async getAllTasks() {
-    const tasks = await this.taskRepository.findAll();
-    return tasks;
+  async changeStatusTask(id: any) {
+    const task = await this.taskRepository.findByPk(id);
+
+    console.log(task);
+
+    await this.taskRepository.update(
+      { isDone: !task.isDone },
+      {
+        where: {
+          id,
+        },
+      },
+    );
+    return await this.taskRepository.findByPk(id); // Находим второй раз для получения актуальных данных
+  }
+
+  async upgradeTask(id: string, dto: CreateTaskDto) {
+    const task = await this.taskRepository.findByPk(id);
+    task.text = dto.text;
+    return task;
   }
 }
